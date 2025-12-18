9ab685a85d22fa1c3bce94281febe1205380000c
Daisy Allen <daisyallen89@comcast.net>
2025-12-18 07:37:29 -0500

    Update Product.cs

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,7 +6,7 @@ namespace MSFD_ProductManagementApp.Models
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int StockQuantity { get; set; }
-
+        
     // Constructor - initializes a new instance of the Product class
         public Product(int id, string name, decimal price, int stockQuantity)
         {
